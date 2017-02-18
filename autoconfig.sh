7a89d68e66f8290391014aac46813488d6e60049
shicky256 <shicky256@gmail.com>
2017-02-18 11:55:50 -0500

    Update autoconfig.sh

--- autoconfig.sh
+++ autoconfig.sh
@@ -1,14 +1,4 @@
 #! /bin/bash
-#autoconfig.sh- tries to connect to a wifi network. if it can't,
-#makes its own AP.
-#requires hostapd and isc-dhcp-server to be installed
-#make sure you run it as root
-#also make sure the interface is commented out in /etc/network/interfaces
-
-#makes sure nothing else messes with the network~
-echo "turning it off and back on..."
-ip link set wlan0 down
-ip link set wlan0 up
 
 #tries to connect to team2537_cs network
 echo "connecting to the network"
@@ -28,13 +18,11 @@ if nc -zw1 192.168.0.1 80; then
 fi
 echo "making an AP now"
 ip link set wlan0 down
-ip link set wlan0 up
+ip link set wlan0 up                                
 ip addr add 10.0.0.1/24 dev wlan0
 sleep 2
 echo "enabling dhcp server"
 dhcpd wlan0 &
 
 echo "enabling AP"
-hostapd hostapd.conf
-killall dhcpd
-
+hostapd /home/pi/hostapd.conf &

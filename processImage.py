1f57ac61dd06b721aea36ee297bf7694917d842d
shicky256 <shicky256@gmail.com>
2017-10-18 23:31:02 -0400

    Update processImage.py

--- processImage.py
+++ processImage.py
@@ -1,10 +1,13 @@
 import cv2
 import numpy as np
-img=cv2.imread('image1.bmp')
+img=cv2.imread('image.bmp')
 height,width,_=img.shape
+print height
+print width
+
 f=open('out.txt','w+')
-for i in range (0,width):
-	for j in range (0,height):
+for i in xrange (0,height):
+	for j in xrange (0,width):
 		if np.any(img[i,j]!=0):
 			f.write('1')
 		else:
